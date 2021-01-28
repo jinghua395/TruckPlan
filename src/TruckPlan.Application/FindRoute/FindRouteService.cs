8816612086020822369954c5aec53fabcd8261f8
Jinghua Wang <jwn@danskecommodities.com>
2021-01-28 21:26:38 +0100

    Completed findrouteservice

---
+++ src/TruckPlan.Application/FindRoute/FindRouteService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruckPlan.Application.FindRoute
+{
+    public class FindRouteService
+    {
+        private readonly IRouteAPIService _routeApiService;
+
+        public FindRouteService(IRouteAPIService routeApiService)
+        {
+            _routeApiService = routeApiService;
+        }
+
+        public async Task<RouteDTO> Handle(decimal longitude, decimal latitude)
+        {
+            var route = await _routeApiService.GetRoute(longitude, latitude);
+
+            return new RouteDTO
+            {
+                Locations = route.Select(r => new LocationDTO
+                {
+                    Longitude = r.Longitude,
+                    Latitude = r.Latitude
+                })
+            };
+        }
+
+        public class RouteDTO
+        {
+            public IEnumerable<LocationDTO> Locations { get; set; }
+        }
+
+        public class LocationDTO
+        {
+            public decimal Latitude { get; set; }
+            public decimal Longitude { get; set; }
+        }
+    }
+}
---
+++ src/TruckPlan.Application/FindRoute/IRouteAPIService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TruckPlan.Application.FindRoute
+{
+    public interface IRouteAPIService
+    {
+        Task<IEnumerable<LocationDTO>> GetRoute(decimal longitude, decimal latitude);
+    }
+
+    /// <summary>
+    /// Use own DTO, since this is usually defined by API
+    /// </summary>
+    public class LocationDTO
+    {
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+}
